ea9f2094a8764bf09443e98673ffdd856ffc2674
kaua2006henrique023@gmail.com <Kaua-Henrique1>
2026-01-02 23:10:31 -0300

    Adicionando conteudo sobre file (Principais metodos e sintaxe)

--- IdeaProjects/maratonaJava/src/academy/devkaua/maratonajava/javacore/Vio/test/FileTest01.java
+++ IdeaProjects/maratonaJava/src/academy/devkaua/maratonajava/javacore/Vio/test/FileTest01.java
@@ -1,4 +1,29 @@
 package academy.devkaua.maratonajava.javacore.Vio.test;
 
+import java.io.File;
+import java.io.IOException;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.util.Date;
+
 public class FileTest01 {
+    public static void main(String[] args) {
+        File file = new File("file.txt");
+        try {
+            boolean isCreated = file.createNewFile();
+            System.out.println(isCreated);
+            boolean exists = file.exists();
+            if (exists) {
+
+                System.out.println("deleted: "+ file.delete());
+                System.out.println("getPath: "+ file.getPath());
+                System.out.println("getAbsolutePath: "+ file.getAbsolutePath());
+                System.out.println("isDirectory: "+ file.isDirectory());
+                System.out.println("isFile: "+ file.isFile());
+                System.out.println("Data de modificacao: "+ Instant.ofEpochMilli(file.lastModified()).atZone(ZoneId.systemDefault()));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }

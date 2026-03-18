fa795e30c9717c13bc0e3c2a19c7c40fac8d1e3f
kaua <kaua2006henrique023@gmail.com>
2026-03-18 09:16:55 -0300

    feat: adiciona log por meio de Log4j2 e anotações por meio do Lombok

--- IdeaProjects/maratonaJava/src/main/java/academy/devkaua/maratonajava/javacore/ZZFjbdc/repository/ProducerRepository.java
+++ IdeaProjects/maratonaJava/src/main/java/academy/devkaua/maratonajava/javacore/ZZFjbdc/repository/ProducerRepository.java
@@ -2,18 +2,23 @@ package academy.devkaua.maratonajava.javacore.ZZFjbdc.repository;
 
 import academy.devkaua.maratonajava.javacore.ZZFjbdc.conn.ConnectionFactory;
 import academy.devkaua.maratonajava.javacore.ZZFjbdc.domain.Producer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.sql.Connection;
 import java.sql.Statement;
 
 public class ProducerRepository {
+    private static final Logger log = LogManager.getLogger(ProducerRepository.class);
+
     public static void save(Producer producer) {
-        String sql = "INSERT INTO producer (name) VALUES ('Kaua');";
+        String sql = "INSERT INTO producer (name) VALUES ('%s');".formatted(producer.getName());
         try (Connection conn = ConnectionFactory.getConnection();
              Statement stmt = conn.createStatement()) {
 
             int i = stmt.executeUpdate(sql);
-            System.out.println(i);
+
+            log.info("Database rows affected{}",i);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
--- IdeaProjects/maratonaJava/src/main/java/academy/devkaua/maratonajava/javacore/ZZFjbdc/test/ConnectionFactoryTest.java
+++ IdeaProjects/maratonaJava/src/main/java/academy/devkaua/maratonajava/javacore/ZZFjbdc/test/ConnectionFactoryTest.java
@@ -2,10 +2,12 @@ package academy.devkaua.maratonajava.javacore.ZZFjbdc.test;
 
 import academy.devkaua.maratonajava.javacore.ZZFjbdc.domain.Producer;
 import academy.devkaua.maratonajava.javacore.ZZFjbdc.repository.ProducerRepository;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 public class ConnectionFactoryTest {
     public static void main(String[] args) {
-        Producer producer = Producer.ProducerBuilder.builder().name("Lucas").build();
+        Producer producer = Producer.builder().name("Studio Pequeno").build();
         ProducerRepository.save(producer);
     }
 }

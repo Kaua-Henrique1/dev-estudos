d03f7e91de53286129536a06d12f45991e2057a0
kaua <kaua2006henrique023@gmail.com>
2026-03-18 12:26:08 -0300

    feat: Crud completo com dados com Statement

--- IdeaProjects/maratonaJava/src/main/java/academy/devkaua/maratonajava/javacore/ZZFjbdc/repository/ProducerRepository.java
+++ IdeaProjects/maratonaJava/src/main/java/academy/devkaua/maratonajava/javacore/ZZFjbdc/repository/ProducerRepository.java
@@ -18,9 +18,36 @@ public class ProducerRepository {
 
             int i = stmt.executeUpdate(sql);
 
-            log.info("Database rows affected{}",i);
+            log.info("Inserted producer '{}' in the Database, rows affected '{}'", producer.getName(), i);
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            log.error("Error while trying to insert producer '{}'", producer.getName(), e);
+        }
+    }
+
+    // "DELETE FROM public.producer WHERE id_producer BETWEEN 3 and 5;";
+    public static void delete(int id) {
+        String sql = "DELETE FROM producer WHERE id_producer = %d;".formatted(id);
+        try (Connection conn = ConnectionFactory.getConnection();
+             Statement stmt = conn.createStatement()) {
+
+            int i = stmt.executeUpdate(sql);
+
+            log.info("Deleted producer '{}' in the Database, rows affected '{}'", id, i);
+        } catch (Exception e) {
+            log.error("Error while trying to delete id '{}'", id, e);
+        }
+    }
+
+    public static void update(Producer producer) {
+        String sql = "UPDATE producer SET name = '%s' WHERE id_producer = %d;".formatted(producer.getName(), producer.getId());
+        try (Connection conn = ConnectionFactory.getConnection();
+             Statement stmt = conn.createStatement()) {
+
+            int i = stmt.executeUpdate(sql);
+
+            log.info("Update ID '{}', rows affected '{}'", producer.getId(), i);
+        } catch (Exception e) {
+            log.error("Error while trying update '{}'", producer.getId(), e);
         }
     }
 }
--- IdeaProjects/maratonaJava/src/main/java/academy/devkaua/maratonajava/javacore/ZZFjbdc/test/ConnectionFactoryTest.java
+++ IdeaProjects/maratonaJava/src/main/java/academy/devkaua/maratonajava/javacore/ZZFjbdc/test/ConnectionFactoryTest.java
@@ -1,13 +1,16 @@
 package academy.devkaua.maratonajava.javacore.ZZFjbdc.test;
 
 import academy.devkaua.maratonajava.javacore.ZZFjbdc.domain.Producer;
-import academy.devkaua.maratonajava.javacore.ZZFjbdc.repository.ProducerRepository;
+import academy.devkaua.maratonajava.javacore.ZZFjbdc.service.ProducerService;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 public class ConnectionFactoryTest {
     public static void main(String[] args) {
         Producer producer = Producer.builder().name("Studio Pequeno").build();
-        ProducerRepository.save(producer);
+        Producer producerToUpdate = Producer.builder().id(1).name("Studio Pequeno").build();
+        // ProducerService.save(producer);
+        // ProducerService.delete(2);
+        ProducerService.update(producerToUpdate);
     }
 }

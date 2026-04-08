6053647e8b3b13e6c9ab239bd087d095f81db03e
kaua <kaua2006henrique023@gmail.com>
2026-04-08 08:21:22 -0300

    feat: adiciona estrutura e configuracao da estrutura

---
+++ dotnet/Model/Cliente.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Company.ClassLibrary1;
+
+public class Cliente
+{
+    private int codigo {get ; set;}
+    private string nome {get ; set;}
+    private string categoria {get ; set;}
+    private string fabricante {get ; set;}
+}
